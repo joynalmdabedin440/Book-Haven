9fe9b41318b757de8b3128656122ea70cde86d8e
joynalmdabedin <joynalmdabedin440@gmail.com>
2025-01-30 20:18:20 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import NavBar from './components/NavBar'
+
+function App() {
+  
+
+  return (
+    <div>
+      <NavBar></NavBar>
+      
+    </div>
+    
+  )
+}
+
+export default App
